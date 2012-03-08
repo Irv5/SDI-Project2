6c86d63442f1c052ffe110aa44e691a010b8f75a
Irvin Reaves <irv5@Irvins-MacBook-Pro.local>
2012-03-08 17:52:14 -0500

    created the Boolen Function

--- Reaves_Irvin_Assignment2.js
+++ Reaves_Irvin_Assignment2.js
@@ -12,3 +12,24 @@ console.log("I have on my " + Black + " black and red "  + Red +  " golf shoes!"
 };
 golfShoes("size 12", "Soft Bottoms");
 
+// Boolen Function
+
+var totalTeeShotDistance = function  (TeeShot1, TeeShot2, TeeShot3){
+	var teeShots = TeeShot1 + TeeShot2 + TeeShot3,
+		combindTeeShotDistance, totalDistance;
+						
+if(teeShots == 764){
+		console.log(true);
+}
+else{
+		console.log(false);
+	}
+	
+	totalDistance = "You had a total driving distance of " + teeShots + " Yards!";
+	
+	return totalDistance;	
+			
+};
+
+var totalDrivingDistance = totalTeeShotDistance(250, 275, 239);
+console.log(totalDrivingDistance);
