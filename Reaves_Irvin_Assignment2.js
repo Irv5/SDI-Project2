bfb680d01b5b49f5fe1202576b8630ce26286e60
Irvin Reaves <irv5@Irvins-MacBook-Pro.local>
2012-03-08 17:20:33 -0500

    updated the .html file, and created the String Function in the .js file.

--- Reaves_Irvin_Assignment2.js
+++ Reaves_Irvin_Assignment2.js
@@ -1,1 +1,14 @@
-alert("JavaScript works!");
+// SDI Project Assignment 2!
+// Author: Irvin Reaves
+// Created for: SDI Online
+
+
+// String Function
+
+var golfShoes = function  (Black, Red){
+	var shoes = "Size 12" + "Soft Bottoms";
+console.log("I have on my " + Black + " black and red "  + Red +  " golf shoes!");
+
+};
+golfShoes("size 12", "Soft Bottoms");
+
